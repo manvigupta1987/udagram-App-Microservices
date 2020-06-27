000eef13e965f04a36d51e80a6253e22e5df3cf6
Manvi <manvigupta1987@gmail.com>
2020-06-26 21:54:54 -0400

    server.ts changes

--- udagram-feed-api/src/server.ts
+++ udagram-feed-api/src/server.ts
@@ -16,21 +16,29 @@ import { V0_FEED_MODELS } from "./controllers/v0/model.index";
   const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", config.url);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Access-Token"
+    );
+    next();
+  });
 
-  app.use(
-    cors({
-      allowedHeaders: [
-        "Origin",
-        "X-Requested-With",
-        "Content-Type",
-        "Accept",
-        "X-Access-Token",
-        "Authorization"
-      ],
-      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-      origin: "*"
-    })
-  );
+  // app.use(
+  //   cors({
+  //     allowedHeaders: [
+  //       "Origin",
+  //       "X-Requested-With",
+  //       "Content-Type",
+  //       "Accept",
+  //       "X-Access-Token",
+  //       "Authorization"
+  //     ],
+  //     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  //     origin: "*"
+  //   })
+  // );
 
   app.use("/api/v0/", IndexRouter);
 
--- udagram-user-api/src/server.ts
+++ udagram-user-api/src/server.ts
@@ -13,24 +13,32 @@ import { V0_USER_MODELS } from "./controllers/v0/model.index";
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", config.url);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Access-Token"
+    );
+    next();
+  });
 
-  app.use(
-    cors({
-      allowedHeaders: [
-        "Origin",
-        "X-Requested-With",
-        "Content-Type",
-        "Accept",
-        "X-Access-Token",
-        "Authorization"
-      ],
-      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-      origin: "*"
-    })
-  );
+  // app.use(
+  //   cors({
+  //     allowedHeaders: [
+  //       "Origin",
+  //       "X-Requested-With",
+  //       "Content-Type",
+  //       "Accept",
+  //       "X-Access-Token",
+  //       "Authorization"
+  //     ],
+  //     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  //     origin: "*"
+  //   })
+  // );
 
   app.use("/api/v0/", IndexRouter);
 
